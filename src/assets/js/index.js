d8ae50f8942f709df35f048f0f4d195ee4d682ff
pawanrathnayake <96866025+pawanrathnayake@users.noreply.github.com>
2022-07-18 13:36:25 +0530

    Edit Js file

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,36 +1,3 @@
-// $(document).on(
-//     "click",
-//     ".section-slk .container .tabs-wrapper .tabs .tab",
-//     function () {
-//       if ($(this).data("target") < $(".is-active").data("target")) {
-//         $(".section-slk .container .tabs-wrapper .tabs .tab").addClass(
-//           "before-slide"
-//         );
-//         $(".section-slk .container .tabs-wrapper .tabs .tab").removeClass(
-//           "after-slide"
-//         );
-//       } else {
-//         $(".section-slk .container .tabs-wrapper .tabs .tab").removeClass(
-//           "before-slide"
-//         );
-//         $(".section-slk .container .tabs-wrapper .tabs .tab").addClass(
-//           "after-slide"
-//         );
-//       }
-  
-//       $(".section-slk .container .tabs-wrapper .tabs .tab").removeClass(
-//         "is-active"
-//       );
-//       $(this).addClass("is-active");
-  
-//       $(".section-slk .active-content .text").removeClass("active-text");
-//       let active_text = $(this).data("target");
-//       $(
-//         `.section-slk .active-content div:nth-child(${parseInt(active_text)})`
-//       ).addClass("active-text");
-//     }
-//   );
-
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
